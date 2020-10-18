9727b11713b1d0771262ab842c160bbea1d3ac75
Dinesh Kumar <dinesh21dinu@gmail.com>
2020-10-18 21:03:39 +0530

    Initial Commint

---
+++ DemoABC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoABC
+//
+//  Created by Dinesh Thakur on 10/18/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        NSLog("ABC");
+    }
+
+
+}
+
